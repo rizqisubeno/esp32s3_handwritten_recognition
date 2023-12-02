c7bc9e780de0b814765eef9a00ab2df3e679ea97
rizqisubeno <rizqisubeno@gmail.com>
2023-12-02 16:52:41 +0700

    delete components

--- components/esp_lcd_touch_xpt2046/include/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTIL__
-#define __UTIL__
-
-double scale(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
-  return (double)((x - in_min) * (out_max - out_min)) / (double)((in_max - in_min)) + (double)out_min;
-}
-
-#endif
